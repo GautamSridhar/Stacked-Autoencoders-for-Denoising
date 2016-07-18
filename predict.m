3975b39d83aaaca64e222633ecf6d5896d801a9e
Gautam Sridhar <gautam.neon@gmail.com>
2016-07-18 16:05:37 +0530

    corrected activation output

--- predict.m
+++ predict.m
@@ -3,13 +3,13 @@ function [activation] = predict(theta, hiddenSizeL1,hiddenSizeL2, visibleSize, d
 q = (hiddenSizeL1*hiddenSizeL2)+(hiddenSizeL1*visibleSize);
 q1 = (hiddenSizeL2*visibleSize); 
 W1 = reshape(theta(1:hiddenSizeL1*visibleSize), hiddenSizeL1, visibleSize);
-W2 = reshape(theta(hiddenSizeL1*visibleSize+1:q), hiddenSizeL1, hiddenSizeL2);
+W2 = reshape(theta(hiddenSizeL1*visibleSize+1:q), hiddenSizeL2, hiddenSizeL1);
 W3 = reshape(theta(q+1:q+q1),visibleSize,hiddenSizeL2);
 b1 = theta(q+q1+1:q+q1+hiddenSizeL1);
 b2 = theta(q+q1+hiddenSizeL1+1:q+q1+hiddenSizeL1+hiddenSizeL2);
 b3 = theta(q+q1+hiddenSizeL1+hiddenSizeL2+1:end);
 
-[activation,~,~,~] = getActivation(W1,W2,W3,b1,b2,b3,data);
+[~,~,~,activation] = getActivation(W1,W2,W3,b1,b2,b3,data);
 % patch_out = reshape(activation,[21,21]);
 
 
